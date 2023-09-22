964ebe5add5960639557672fb75b98b983d311c0
InteractiveWebdesign <ruud.weijters@outlook.com>
2023-09-22 19:27:09 +0200

    les 8 Add Table to Index Pages compleet

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -7,8 +7,55 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            Projects
-            
+
+            <div class="overflow-x-auto relative shadow-md sm:rounded-lg" bis_skin_checked="1">
+                <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                    <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                        <tr>
+                            <th scope="col" class="px-6 py-3">
+                                Name
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                Skill
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                Image
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @forelse ($projects as $project)
+                        <tr class="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
+                            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                {{ $project->name }}
+                                
+                            </th>
+                            <td class="px-6 py-4">
+                                {{ $project->skill->name }}
+                            </td>
+                            <td class="px-6 py-4">
+                                Image
+                            </td>
+                            <td class="px-6 py-4">
+                                <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                            </td>
+                        </tr>
+
+                        @empty
+                        <tr>
+                            <td colspan="4" class="px-6 py-4">
+                                No projects found.
+                            </td>
+                        </tr>
+                        
+                        @endforelse
+                        
+                    </tbody>
+                </table>
+            </div>
         </div>
     </div>
 </x-app-layout>
--- resources/views/skills/index.blade.php
+++ resources/views/skills/index.blade.php
@@ -7,8 +7,46 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            Skills
-            
+            <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                    <tr>
+                        <th scope="col" class="px-6 py-3">
+                            Name
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                            Image
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                            
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @forelse ($skills as $skill)
+                    <tr class="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
+                        <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                            {{ $skill->name }}
+                            
+                        </th>
+                        <td class="px-6 py-4">
+                            Image
+                        </td>
+                        <td class="px-6 py-4">
+                            <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                        </td>
+                    </tr>
+
+                    @empty
+                    <tr>
+                        <td colspan="4" class="px-6 py-4">
+                            No skills found.
+                        </td>
+                    </tr>
+                    
+                    @endforelse
+                    
+                </tbody>
+            </table>
         </div>
     </div>
 </x-app-layout>
